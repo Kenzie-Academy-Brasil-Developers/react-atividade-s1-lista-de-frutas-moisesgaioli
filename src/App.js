4a3c45b2af5f3c390db4825a4a20ce03e4f319dc
Moises Gaioli Estevão <moi.rp@hotmail.com>
2021-09-02 16:50:26 -0300

    Finalizado

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import { useState } from "react";
+
+
+function App() {
+
+  const [fruits, setFruits] = useState([ 
+    {
+      name: "banana", 
+      color: "yellow", 
+      price: 2,
+    },
+    {
+      name: "cherry",
+      color: "red",
+      price: 3,
+    },
+    {
+      name: "strawberry", 
+      color: "red", 
+      price: 4,
+    },
+  ])
+
+  const filterRedFruits = () => {
+
+    setFruits(fruits.filter(item => item.color === "red"))
+  }
+
+  const totalPrice = fruits.reduce((acc, elem) => acc + elem.price, 0)
+  
+  return (
+    <div className="App">
+
+    <h3 className="title">Preço total = {totalPrice}</h3>
+    
+    <ul className="list">
+      {fruits.map((item, index) => <li key={index}>{item.name}</li> )}
+    </ul>
+
+    <button className="button" onClick={filterRedFruits}> Filtrar frutas vermelhas </button>
+
+    </div>
+  );
+}
+
+export default App;
